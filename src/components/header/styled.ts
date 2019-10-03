27e60ed847f8e7011e00c729f40a93da7a9500fb
Constantine Lebedev <koss.lebedev@gmail.com>
2019-10-03 13:36:09 +0200

    Fix broken layout on mobile and tablet

--- src/components/header/styled.ts
+++ src/components/header/styled.ts
@@ -1,6 +1,7 @@
 import { Link } from "gatsby"
 import styled from '@emotion/styled'
 import theme from 'styles/theme'
+import mq from 'styles/mq'
 
 export const Wrapper = styled.header`
   position: absolute;
@@ -8,6 +9,11 @@ export const Wrapper = styled.header`
   right: 0;
   height: 6rem;
   z-index: 1;
+  background: white;
+
+  ${mq.medium} {
+    background: none;
+  }
 `
 
 export const Inner = styled.div`
@@ -17,15 +23,28 @@ export const Inner = styled.div`
   align-items: center;
   height: 100%;
   justify-content: space-between;
+
+  ${mq.medium} {
+    padding: 0 4rem;
+  }
 `
 
 export const Logo = styled(Link)`
   font-weight: bold;
   text-decoration: none;
   color: ${theme.color.textDark};
+  width: 100%;
+  text-align: center;
+
+  ${mq.medium} {
+    width: unset;
+    text-align: left;
+  }
 `
 
 export const Nav = styled.nav`
+  display: none;
+
   ul {
     list-style: none;
     padding: 0;
@@ -43,5 +62,9 @@ export const Nav = styled.nav`
         }
       }
     }
+  }
+
+  ${mq.medium} {
+    display: block;
   }
 `
